01ba400f0788846d79ec9a57cacd8116be595d28
huying <2369700690@qq.com>
2022-01-18 11:14:14 +0800

    Unfold.vue

---
+++ 06.点击折叠展开收起/unfold.vue
@@ -0,0 +1,62 @@
+<template>
+  <span>
+    <span v-if="data.length<maxLen">
+      <span class="tj">{{ data }}</span>
+    </span>
+    <span v-else>
+      <span class="tj">{{ showBtn?sliceStr:data }}
+        <span class="btnWord" @click="showBtn=!showBtn">{{ showBtn?tip:"收起" }}</span>
+      </span>
+    </span>
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'Unfold',
+  props: {
+    tip: {
+      type: String,
+      default: '展开'
+    },
+    // 数据
+    data: {
+      type: String,
+      default: ''
+    },
+    // 最大长度
+    maxLen: {
+      type: Number,
+      default: 100
+    }
+  },
+  data() {
+    return {
+      showBtn: true
+    }
+  },
+  computed: {
+    sliceStr() {
+      if (this.data != null) {
+        return this.data.substring(0, this.maxLen) + '...'
+      }
+      return ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.tj {
+    text-align: justify;
+    line-height: 25px;
+    display: inline-block;
+}
+
+.btnWord {
+    color: cornflowerblue;
+    cursor: pointer;
+    word-break: keep-all;
+}
+</style>
+
--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # 整理奇怪需求集合
 
-### 1.PC端获取当前经纬度 
+### PC端获取当前经纬度 
 
 - 使用高德地图开发文档
 - 唤醒浏览器定位权限
 
-### 2.canvas画点连线
+### canvas画点连线
 
 - Setting.jsx --- 父组件
    DragArea.jsx --- 四个点可拖拽 画点连线
@@ -14,7 +14,9 @@
 - Setting.vue --- 父组件
    CanvasDisplay.vue --- 子组件 可拖拽 画点连线
 
-### 5.loading框
+### loading框
 
-- 优化全局按钮 
+### 点击折叠展开收起 `Unfold.vue`
+
+
 
