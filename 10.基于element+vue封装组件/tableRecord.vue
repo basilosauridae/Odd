35b864f88d49f9b4b82f084d93034f133a6c8418
huying <2369700690@qq.com>
2022-06-22 11:07:46 +0800

    基于element+vue封装的组件

---
+++ 10.基于element+vue封装组件/tableRecord.vue
@@ -0,0 +1,56 @@
+<template>
+  <el-table
+    :data="tableData"
+    :header-cell-style="{
+      'background-color': '#f5f7fa',
+    }"
+    :cell-style="{
+      'border':'1px solid transparent',
+      'padding':'5px 0px'
+    }"
+    fit
+  >
+    <el-table-column
+      type="index"
+      width="50"
+      align="center"
+    >
+    </el-table-column>
+    <el-table-column
+      align="center"
+      v-for="(item, index) in tableHeader"
+      :key="index"
+      :prop="item.prop"
+      :label="item.label"
+      :width="item.width"
+      :show-overflow-tooltip="true"
+    >
+    </el-table-column>
+    <slot name="back"></slot>
+  </el-table>
+</template>
+
+<script>
+export default {
+  name: 'NoBorderTable',
+  props: {
+    tableData: {
+      type: Array,
+      required: true
+    },
+    tableHeader: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+.el-table::before {
+  z-index: inherit;
+}
+.el-table--fit {
+  border-bottom: 1px solid #dfe6ec;
+}
+</style>
--- README.md
+++ README.md
@@ -1,26 +1,30 @@
 # 整理奇怪需求集合
 
-### PC端获取当前经纬度 
+### PC 端获取当前经纬度
 
 - 使用高德地图开发文档
 - 唤醒浏览器定位权限
 
-### canvas画点连线
+### canvas 画点连线
 
 - Setting.jsx --- 父组件
-   DragArea.jsx --- 四个点可拖拽 画点连线
-   DragLineArea.jsx --- 两点连线
-   DrawRectArea.jsx --- 矩形可拖动
+  DragArea.jsx --- 四个点可拖拽 画点连线
+  DragLineArea.jsx --- 两点连线
+  DrawRectArea.jsx --- 矩形可拖动
 - Setting.vue --- 父组件
-   CanvasDisplay.vue --- 子组件 可拖拽 画点连线
+  CanvasDisplay.vue --- 子组件 可拖拽 画点连线
 
-### loading框
+### loading 框
 
 ### 点击折叠展开收起 `Unfold.vue`
 
-### file-upload关于上传
+### file-upload 关于上传
 
-el-upload基于文件流、base64和断点续传三个方式
+el-upload 基于文件流、base64 和断点续传三个方式
 
+### 56 个手写 Javascript 知识
 
+### 基于element+vue封装的一些组件
+
+- tableRecord - 简单的无法修改的el-table
 
